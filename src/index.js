52f0f4e744f5583be6a20e3267aee078a2cebbe9
Ivan Khailo <jakom333@gmail.com>
2021-02-03 00:39:54 +0200

    homework for chrcking

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import "./styles.css";
+import fetchCountries from "./js/fetchCountries.js";
+import debounce from "lodash.debounce";
+const input = document.querySelector("input");
 
+input.addEventListener("input", debounce(search, 500));
+
+function search(event) {
+  fetchCountries.fetchCountries(event.target.value);
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,37 @@
-export default function fetchCountries(searchQuery) {}
+import countries from "../templates/countries.hbs";
+import countriesList from "../templates/countries-list.hbs";
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
+const container = document.querySelector(".container");
+export default {
+  fetchCountries(searchQuery) {
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => markup(data))
+      .catch((error) => console.log(error));
+  },
+};
+
+function markup(data) {
+  if (data.length >= 2 && data.length <= 10) {
+    container.innerHTML = countriesList(data);
+  } else if (data.length > 10) {
+    container.innerHTML = "";
+    resultMessage(error, "Слишком много стран");
+  } else {
+    const countryMarkup = data.map((el) => countries(el)).join("");
+    container.innerHTML = countryMarkup;
+  }
+}
+
+function resultMessage(typeInfo, textInfo) {
+  typeInfo({
+    text: `${textInfo}`,
+    delay: 1000,
+    closerHover: true,
+  });
+}
